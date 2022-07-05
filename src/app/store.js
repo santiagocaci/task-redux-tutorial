dbf47932c5b1590ebe34a58028bc6c6ec241fe03
Santiago Caci <santiago.caci@gmail.com>
2022-07-05 16:32:55 -0300

    Archivos innecesarios borrados

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
-import postsReducer from '../features/posts/postsSlice';
 import taskReducer from '../features/tasks/taskSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
-    posts: postsReducer,
     tasks: taskReducer,
   },
 });
